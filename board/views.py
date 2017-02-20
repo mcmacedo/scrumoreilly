77fa38fce8f467422f43059208e3086d42461e56
Matheus Macedo <mcmacedoprog@outlook.com>
2017-02-20 20:50:19 -0300

    Views e Serializadores implementados.

--- board/views.py
+++ board/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import authentication, permissions, viewsets
 
-# Create your views here.
+from .models import Sprint, Task
+from .serializers import SprintSerializer,TaskSerializer,UserSerializer
+
+
+User = get_user_model()
+
+class DefaultMixin(object):
+    """Configurações default para autenticação, permissão, filtragem
+    e paginação das views."""
+
+    authentication_classes = (
+        authentication.BaseAuthentication,
+        authentication.TokenAuthentication,
+    )
+
+    permissions_classes = (
+        permissions.IsAuthenticated,
+    )
+
+    paginate_by = 25
+    paginate_by_param = 'page_sizes'
+    max_paginate_by = 100
+
+
+class UserViewSet(DefaultMixin, viewsets.ReadOnlyModelViewSet):
+    """Edpoint da API para listar usuários."""
+
+    lookup_field = User.USERNAME_FIELD
+    lookup_url_kwarg = User.USERNAME_FIELD
+    queryset = User.objects.order_by(User.USERNAME_FIELD)
+    serializer_class = UserSerializer
+
+
+class TaskViewSet(DefaultMixin, viewsets.ModelViewSet):
+    """Endpoint da API para listar e criar tarefas."""
+
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+
+
+class SprintViewSet(DefaultMixin, viewsets.ModelViewSet):
+    """Endpoint da API para listar e criar sprints."""
+
+    queryset = Sprint.objects.order_by('end')
+    serializer_class = SprintSerializer
+
